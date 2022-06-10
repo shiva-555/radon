49a0b2ca7eb8dd1c382a27b7f076ef9f98f9daba
shiva-555 <shivkinga@gmail.com>
2022-06-11 00:47:29 +0530

    new assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+const assignmentMW = function (req,res,next){
+    var currentdate = new Date();
+    var datetime = "Last Sync: " + currentdate.getDay() + "/" + 
+    currentdate.getMonth()  + "/" 
+    + currentdate.getFullYear() + " @ " 
+    + currentdate.getHours() + ":" 
+    + currentdate.getMinutes() + ":"
+     + currentdate.getSeconds();
+     let ip = req.ip
+     let url= req.originalUrl
+     console.log(`${datetime} ${ip} ${url}`)
+     next()
+ }
+ app.use(assignmentMW)
+ 
+
+
+
+
+mongoose.connect("mongodb+srv://Shiva-555:pBG3QBG7xnjaNHcZ@cluster0.zkrrd.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
